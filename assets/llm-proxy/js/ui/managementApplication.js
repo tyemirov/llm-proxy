31261873e3035f338ff5b20367df52dcacec8545
MPR Lab Gateway <gateway@mprlab.invalid>
2026-08-20 02:31:20 +0000

    Publish v4.0.0

---
+++ assets/llm-proxy/js/ui/managementApplication.js
@@ -0,0 +1,61 @@
+// @ts-check
+
+import { APP_INTEGRITY_ERROR } from "../constants.js?v=20260811c131";
+import { createAdminDashboardResponsibility } from "./adminDashboard.js?v=20260811c131";
+import { createAuthenticationLifecycleResponsibility } from "./authenticationLifecycle.js?v=20260811c131";
+import { createClientAccessResponsibility } from "./clientAccess.js?v=20260811c131";
+import { createManagementApplicationPresentationResponsibility } from "./managementApplicationPresentation.js?v=20260811c131";
+import { createManagementApplicationState } from "./managementApplicationState.js?v=20260811c131";
+import { createNotificationsResponsibility } from "./notifications.js?v=20260811c131";
+import { createProfileMutationsResponsibility } from "./profileMutations.js?v=20260811c131";
+import { createProviderCredentialsResponsibility } from "./providerCredentials.js?v=20260811c131";
+import { createProviderEditorResponsibility } from "./providerEditor.js?v=20260811c131";
+import { createProviderSettingsResponsibility } from "./providerSettings.js?v=20260811c131";
+import { createRequestExamplesResponsibility } from "./requestExamples.js?v=20260811c131";
+import { createRoutingDefaultsResponsibility } from "./routingDefaults.js?v=20260811c131";
+import { createSettingsDialogResponsibility } from "./settingsDialog.js?v=20260811c131";
+import { createTenantSettingsResponsibility } from "./tenantSettings.js?v=20260811c131";
+import { createUsageDashboardResponsibility } from "./usageDashboard.js?v=20260811c131";
+
+/**
+ * Compose the authenticated management application from non-overlapping UI responsibilities.
+ *
+ * @returns {object}
+ */
+export function createManagementApplication() {
+  return composeManagementApplication(
+    createManagementApplicationState(),
+    createManagementApplicationPresentationResponsibility(),
+    createAuthenticationLifecycleResponsibility(),
+    createNotificationsResponsibility(),
+    createProfileMutationsResponsibility(),
+    createTenantSettingsResponsibility(),
+    createUsageDashboardResponsibility(),
+    createAdminDashboardResponsibility(),
+    createSettingsDialogResponsibility(),
+    createProviderEditorResponsibility(),
+    createProviderCredentialsResponsibility(),
+    createProviderSettingsResponsibility(),
+    createRoutingDefaultsResponsibility(),
+    createClientAccessResponsibility(),
+    createRequestExamplesResponsibility(),
+  );
+}
+
+/**
+ * @param {...object} responsibilities
+ * @returns {object}
+ */
+function composeManagementApplication(...responsibilities) {
+  const application = {};
+  for (const responsibility of responsibilities) {
+    const descriptors = Object.getOwnPropertyDescriptors(responsibility);
+    for (const propertyName of Object.keys(descriptors)) {
+      if (Object.hasOwn(application, propertyName)) {
+        throw new Error(`${APP_INTEGRITY_ERROR}:duplicate_management_application_property:${propertyName}`);
+      }
+    }
+    Object.defineProperties(application, descriptors);
+  }
+  return application;
+}
