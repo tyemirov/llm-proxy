249471951f0620a16b29637c37f48266f92a05fa
Vadym Tyemirov <temirov@gmail.com>
2026-09-04 17:49:35 -0700

    style: format OpenAPI contract test case

--- internal/proxy/openapi_contract_test.go
+++ internal/proxy/openapi_contract_test.go
@@ -105,7 +105,8 @@ func TestOpenAPIContractDocumentsActualAuthenticationBoundaries(t *testing.T) {
 			expectedSecurity = [][]string{{"TenantClientKey"}}
 		case "/v1/chat/completions", "/v1/responses", "/v1/models", "/v1/audio/transcriptions":
 			expectedSecurity = [][]string{{"TenantBearerKey"}}
-		case "/healthz", proxy.ManagementConfigUIPath, proxy.PublicCapabilitiesPath:			expectedSecurity = [][]string{}
+		case "/healthz", proxy.ManagementConfigUIPath, proxy.PublicCapabilitiesPath:
+			expectedSecurity = [][]string{}
 		}
 		actualSecurity, securityError := contract.SecurityRequirements(operation.Path, operation.Method)
 		if securityError != nil {
