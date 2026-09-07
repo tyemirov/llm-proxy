e978d9244a9c9b4c802f19f725dc30565679aeff
Vadym Tyemirov <temirov@gmail.com>
2026-09-07 14:17:04 -0700

    test: add globalSetup and TS check to Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,10 @@
+// @ts-check
+
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests/e2e",
+  globalSetup: "./tests/e2e/globalSetup.js",
   timeout: 30000,
   reporter: "list",
   use: {
