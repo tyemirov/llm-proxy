6ded08547fd173b82b68fc720f35e93acb0b7ead
Vadym Tyemirov <temirov@gmail.com>
2026-09-08 11:43:20 -0700

    refactor!: remove Google credential profiles configuration
    - remove GoogleCredentialProfiles from file and proxy configuration
    - delete Google credentials handling in favor of Vertex API key auth
    - update provider catalog, Vertex integration and docs accordingly

--- cmd/cli/config_file.go
+++ cmd/cli/config_file.go
@@ -37,9 +37,8 @@ var (
 )
 
 type fileConfiguration struct {
-	GoogleCredentialProfiles []proxy.GoogleCredentialProfile `mapstructure:"google_credential_profiles"`
-	Server                   serverConfiguration             `mapstructure:"server"`
-	Management               managementConfiguration         `mapstructure:"management"`
+	Server     serverConfiguration     `mapstructure:"server"`
+	Management managementConfiguration `mapstructure:"management"`
 }
 
 type serverConfiguration struct {
@@ -228,7 +227,6 @@ func (configuration fileConfiguration) toProxyConfiguration(providerCatalog *pro
 		Management:               managementProxyConfiguration(configuration.Management, usageQueueSize),
 		ProviderCatalog:          providerCatalog,
 		ProviderConnectionValues: providerConnectionValues,
-		GoogleCredentialProfiles: configuration.GoogleCredentialProfiles,
 		Port:                     configuration.Server.Port,
 		LogLevel:                 configuration.Server.LogLevel,
 		WorkerCount:              configuration.Server.Workers,
